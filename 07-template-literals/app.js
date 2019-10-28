6d803fbe373574aa05136490dc494c7b02787dc9
Jubari <jubari.code@gmail.com>
2019-10-29 00:20:24 +0700

    Modern JavaScript Tutorial - 08 Membuat Element HTML dengan Template Strings

---
+++ 07-template-literals/app.js
@@ -0,0 +1,36 @@
+const name = 'Jubari';
+const age = 19;
+const job = 'Web Developer';
+const city = 'Batam';
+
+let html;
+
+// Without Template String (es5)
+
+html = ' <ul><li>Name: ' + name + '</li><li>Age: ' + age + 
+'</li><li>Job: ' + job +  '</li><li>City: ' + city +'</li> </ul>'  ;
+
+html = '<ul>' + 
+            '<li>Name : ' + name + '</li>' +
+            '<li>Age :' + age + '</li>' +
+            '<li>Job : ' + job + '</li>' +
+            '<li>City : ' + city + '</li>' +
+        '</ul>';
+
+function hello(){
+    return 'Hello';
+}
+// With Template String (es6)
+html =`<ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City : ${city}</li>
+    <li>${5+4}</li>
+    <li>${hello()}</li>
+    <li>${age > 20 ? 'Over 20' : 'Under 20'}</li>
+
+</ul>
+`;
+
+document.body.innerHTML = html;
