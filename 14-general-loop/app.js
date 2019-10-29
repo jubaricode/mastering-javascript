7783067e571ea4c053a0392781b21d561981e6a9
jubaricode <43369339+jubaricode@users.noreply.github.com>
2019-10-29 12:03:05 +0700

    Modern JavaScript Tutorial - 16 Perulangan dalam JavaScript menggunakan Loop

---
+++ 14-general-loop/app.js
@@ -0,0 +1,30 @@
+// FOR LOOP
+
+// for (let i = 0; i < 10; i++) {
+//     if (i === 2) {
+//         console.log('2 is my favorite number');
+//     }
+
+//     if (i === 5) {
+//         console.log('Stop Looping');
+
+//     }
+//     console.log('Number ' + i);
+// }
+
+// // While Loop
+
+// let i = 0;
+
+// while (i < 10) {
+//     console.log('Number ' + i);
+//     i++
+// }
+
+// Do while
+
+let i = 0;
+do {
+    console.log('Number ' + i);
+    i++
+} while (i < 10);
