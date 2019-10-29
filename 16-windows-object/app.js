220f4e33b6a29f9f65f46a50d62f546cdbd88069
Jubari <jubari.code@gmail.com>
2019-10-29 18:27:05 +0700

    Modern JavaScript Tutorial - 18 Mengenal Window Object lebih dalam JavaScript

---
+++ 16-windows-object/app.js
@@ -0,0 +1,29 @@
+// Window Object | Properties | Methods
+
+//window.console.log(123);
+
+// window.alert("Hello");
+
+// prompt
+
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if(confirm('Are you sure?')){
+//     console.log('yes');
+// } else {
+//     console.log('No');
+// }
+
+// let val;
+// val = window.outerHeight;
+// val = window.outerWidth;
+// console.log(val);
+
+// Location Object
+val = window.location;
+
+// History Object
+// val = window.history.go(-3);
+console.log(val);
