9b2afc0233287aaf70d575148bc505df8543d55a
jubaricode <43369339+jubaricode@users.noreply.github.com>
2019-10-29 10:52:27 +0700

    Modern JavaScript Tutorial - 12 Statement If dan Operator Pembanding Bag 1

---
+++ 11-if-statement-operator/app.js
@@ -0,0 +1,41 @@
+// if (true){
+//     do something();
+// }else{
+//     do something();
+// }
+
+const id = '200';
+
+// // EQUAL to
+
+// if (id == 100) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO
+// if (id != 100) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
+
+// EQUAL TO VALUE & TYPE
+// if (id === '100') {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
+
+// if (typeof id != 'undefined') {
+//     console.log(`ID value is: ${id}`);
+// } else {
+//     console.log('ID Value is NULL');
+// }
+
+// //Uncaught TypeError: Assignment to constant variable.
+// if (id = 200) {
+//     console.log('CORRECt');
+// }
+
